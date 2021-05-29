70704e794d4d48b76e7ad26c5a67e0751343cf13
hp <hp@DESKTOP-S4P3ABE>
2021-05-29 16:52:22 -0700

    Create Authentication Tables Using Initial Create Migration

---
+++ OnlineQuizzes/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace OnlineQuizzes.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OnlineQuizzes.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "OnlineQuizzes.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(OnlineQuizzes.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
