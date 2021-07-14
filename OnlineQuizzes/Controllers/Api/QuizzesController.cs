81fdd2a022cce6ee613bec9fed78cc0c1b626545
asy085@usal.edu.lb <asy085@usal.edu.lb>
2021-07-14 22:23:48 +0300

    Final Submission

---
+++ OnlineQuizzes/Controllers/Api/QuizzesController.cs
@@ -0,0 +1,27 @@
+﻿using OnlineQuizzes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OnlineQuizzes.Controllers.Api
+{
+    public class QuizzesController : ApiController
+    {
+        private ApplicationDbContext db;
+
+        public QuizzesController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        [HttpGet]
+        public IEnumerable<Quiz> GetStudents()
+        {
+            return db.Quizzes.ToList();
+        }
+
+    }
+}
---
+++ OnlineQuizzes/Controllers/Api/StudentGradesController.cs
@@ -0,0 +1,26 @@
+﻿using OnlineQuizzes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OnlineQuizzes.Controllers.Api
+{
+    public class StudentGradesController : ApiController
+    {
+        private ApplicationDbContext db;
+
+        public StudentGradesController()
+        {
+            db = new ApplicationDbContext();
+        }
+        [HttpGet]
+        public IEnumerable<Student_Grade> GetStudents()
+        {
+            return db.StudentGrades.ToList();
+        }
+
+    }
+}
